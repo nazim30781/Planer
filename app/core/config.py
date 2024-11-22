1020f2acb51b5fd8e8047738c51af0e8c9afa7ec
nazim30781 <nazim30781@gmail.com>
2024-11-22 20:29:42 +0300

    Created main settings

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+from pydantic.v1 import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+
+class Setting(BaseSettings):
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+
+settings = Setting()
