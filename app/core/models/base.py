610a08d16ec365349b192e11ceeaa00d53ec56f7
nazim30781 <nazim30781@gmail.com>
2024-11-23 11:27:16 +0300

    change declarative base

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, declared_attr
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    @declared_attr
+    @declared_attr.directive
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
 
