b5bbb0fa2c0a3b7d65ebdde1ab8ac57703ea1a1d
nazim30781 <nazim30781@gmail.com>
2024-11-23 20:10:24 +0300

    add User model import

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -8,6 +8,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from core.config import settings
+
+from api.users.models import User
 from core.models import Base
 
 # this is the Alembic Config object, which provides
