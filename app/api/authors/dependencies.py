d29337cd12b2b60e00845675e030a18d6f8a7a66
nazim30781 <nazim30781@gmail.com>
2024-11-29 19:52:27 +0300

    add author_products dependencies

--- app/api/authors/dependencies.py
+++ app/api/authors/dependencies.py
@@ -4,6 +4,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from api import Author, Product, ProductDate
 from api.users.dependencies import access_token_bearer, get_current_user_id
 from api.users.models import User
 from core.models import db_helper
@@ -27,7 +28,7 @@ async def get_author_id(
 
 async def get_author(
         session: AsyncSession = Depends(db_helper.session_getter),
-        user_id: dict = Depends(get_current_user_id)
+        user_id: int = Depends(get_current_user_id)
 ):
     query = (select(User)
              .options(joinedload(User.author))
@@ -38,3 +39,18 @@ async def get_author(
     author_id = user.first()[0].author
 
     return author_id
+
+
+async def get_author_with_products(
+        session: AsyncSession = Depends(db_helper.session_getter),
+        user_id: int = Depends(get_current_user_id)
+):
+    query = (select(Author).
+             options(joinedload(Author.products).
+                     joinedload(Product.dates).
+                     joinedload(ProductDate.times)).
+             where(Author.user_id==user_id))
+
+    result = await session.execute(query)
+
+    return result.first()[0]
